1d888793fc01f04104f263255760a70231b50047
Brennan Brown <brennan.brown@edu.sait.ca>
2020-06-02 18:49:01 -0600

    Add initial API endpoint

--- routes/feedback.js
+++ routes/feedback.js
@@ -4,6 +4,30 @@ const { check, validationResult } = require("express-validator");
 
 const router = express.Router();
 
+// Moving validations into it's own object to be used multiple times.
+const validations = [
+    check("name")
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage("A name is required"),
+    check("email")
+        .trim()
+        .isEmail()
+        .normalizeEmail()
+        .withMessage("A valid email address is required"),
+    check("title")
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage("A title is required"),
+    check("message")
+        .trim()
+        .isLength({ min: 5 })
+        .escape()
+        .withMessage("A message is required"),
+];
+
 module.exports = params => {
     const { feedbackService } = params;
 
@@ -29,31 +53,8 @@ module.exports = params => {
         }
     });
 
-    router.post(
-        "/",
-        [
-            check("name")
-                .trim()
-                .isLength({ min: 3 })
-                .escape()
-                .withMessage("A name is required"),
-            check("email")
-                .trim()
-                .isEmail()
-                .normalizeEmail()
-                .withMessage("A valid email address is required"),
-            check("title")
-                .trim()
-                .isLength({ min: 3 })
-                .escape()
-                .withMessage("A title is required"),
-            check("message")
-                .trim()
-                .isLength({ min: 5 })
-                .escape()
-                .withMessage("A message is required"),
-        ],
-        async (request, response) => {
+    router.post("/", validations, async (request, response, next) => {
+        try {
             const errors = validationResult(request);
 
             if (!errors.isEmpty()) {
@@ -69,8 +70,30 @@ module.exports = params => {
                 message: "Thank you for your feedback!",
             };
             return response.redirect("/feedback");
+        } catch (err) {
+            return next(err);
         }
-    );
+    });
+
+    // Requires a service like Postman for testing.
+    // eg. POST http://localhost:300/feedback/api
+
+    router.post("/api", validations, async (request, response, next) => {
+        try {
+            const errors = validationResult(request);
+            // Additional error-handling since this is not within the browser.
+            if (!errors.isEmpty()) {
+                return response.json({ errors: errors.array() });
+            }
+            const { name, email, title, message } = request.body;
+            await feedbackService.addEntry(name, email, title, message);
+            // Returns the updated list of feedback entries.
+            const feedback = await feedbackService.getList();
+            return response.json({ feedback });
+        } catch (err) {
+            return next(err);
+        }
+    });
 
     return router;
 };
--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(
 );
 
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.json());
 
 /**
  * Routes Definitions
